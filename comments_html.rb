e5f503d19dc53dba052c2f13c527921aa5754a46
Judka Linkov <judka@uriel.world>
2025-02-02 08:29:32 +0200

    Use x-show for inner comment since it's hidden behind x-if

--- comments_html.rb
+++ comments_html.rb
@@ -7,9 +7,9 @@ output = input.gsub(/<p>{OPEN}<\/p>\n([^{}]+?)\n<p>{CLOSE}<\/p>/m) do |_match|
   content = $1
   open_p = content =~ /id="postmodern"|id="typography"/ ? 'true' : 'false'
   <<-HTML.chomp
-<template x-if="buttons"><div class="comments" x-data="{ open: #{open_p} }"><button @click="open = !open" @openall.window="open = true">…</button><div class="comment hidden" :class="{ \'hidden\': !open }">#{content}</div></div></template>
+<template x-if="buttons"><div class="comments" x-data="{ open: #{open_p} }"><button @click="open = !open" @openall.window="open = true">…</button><div class="comment" x-show="open">#{content}</div></div></template>
 HTML
 end.gsub(/{OPEN}(.+?){CLOSE}/m,
-  '<template x-if="buttons"><span class="comments" x-data="{ open: false }"><button @click="open = !open" @openall.window="open = true">…</button><span class="comment hidden" :class="{ \'hidden\': !open }">\1</span></span></template>')
+  '<template x-if="buttons"><span class="comments" x-data="{ open: false }"><button @click="open = !open" @openall.window="open = true">…</button><span class="comment" x-show="open">\1</span></span></template>')
 
 print output
